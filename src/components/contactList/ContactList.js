1036a64465829c27d519e01ae9efa44fa3284def
IrinaMai <irinam3340@gmail.com>
2021-02-02 15:32:19 +0300

    filter chng

--- src/components/contactList/ContactList.js
+++ src/components/contactList/ContactList.js
@@ -1,75 +1,110 @@
-import React, {useEffect} from 'react';
-import { connect } from "react-redux";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { filterHndl, deletBtnHndl, getContactFromLS} from '../../redux/actions/phoneBookAction';
-import { title, input, label, button, list, listItem, nameItem, filter } from './ContactList.module.css';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import {
+  filterHndl,
+  deletBtnHndl,
+  getContactFromLS,
+} from '../../redux/actions/phoneBookAction';
+import {
+  title,
+  input,
+  label,
+  button,
+  list,
+  listItem,
+  nameItem,
+  filter,
+} from './ContactList.module.css';
 import fadeStyle from './fadeStyle.module.css';
 
+const initialFilter = '';
 
-const ContactList = ({ showList, filterHndl, contacts, deletBtnHndl, getContactFromLS }) => {
-      useEffect(() => {
-        if (localStorage.getItem('contacts')) {
-            const listLS = localStorage.getItem('contacts');
-            getContactFromLS(JSON.parse(listLS));
-        }
-    }, [])
+const ContactList = ({
+  showList,
+  filterHndl,
+  contacts,
+  deletBtnHndl,
+  getContactFromLS,
+}) => {
+  //   const [filter, setFilter] = useState();
 
-    const onFilterChng = (e) => {
-        filterHndl(e.target.value);
-    };
-
-    const onDeletBtn = (e) => {
-        deletBtnHndl(e.target.id);
-            
-    };
-
-    
-    return (
-        <>
-             {(contacts.length > 2) && 
-             <label className={label}>Search name:
-             <input name="search" type="text" onChange={onFilterChng} className={input} />
-             </label>}
- 
-            <h2 className={title}>Contact list </h2>
-             <TransitionGroup component="ul" className={list}>
-                {showList.map(({ id, name, phone }) =>
-                <CSSTransition
-                timeout={250}
-                classNames={fadeStyle}
-                key={id}
-                >
-                    <li className={listItem} > 
-                    <span className={nameItem}>{name} :</span>{phone}
-                    <button type="button" onClick={onDeletBtn} id={id} className={button}>Delete</button>
-                    </li>
-                </CSSTransition>
-            )}
-        </TransitionGroup>
-        </>
-        
-    );
-}
-// const getValue = (showList) => {
-//     return (showList.length > 1) ? state.filter : ''
-// }
-
-const mapStateToProps = (state) => {
-    return {
-        showList: state.contactList.filter(item => item.name.toLowerCase().includes(state.filter.toLowerCase())),
-        // filter: (contactList.length >1) ? state.filter : '',
-        // filter: getValue(state),
-        filter: state.filter,
-        contacts: state.contactList,
+  useEffect(() => {
+    if (localStorage.getItem('contacts')) {
+      const listLS = localStorage.getItem('contacts');
+      getContactFromLS(JSON.parse(listLS));
     }
+  }, []);
+
+  useEffect(() => {
+    if (showList.length < 1) {
+      filterHndl('');
+      //   setFilter(initialFilter);
+    }
+  }, [showList.length]);
+
+  const onFilterChng = e => {
+    filterHndl(e.target.value);
+  };
+
+  const onDeletBtn = e => {
+    deletBtnHndl(e.target.id);
+  };
+
+  return (
+    <>
+      {contacts.length > 2 && (
+        <label className={label}>
+          Search name:
+          <input
+            name="search"
+            type="text"
+            onChange={onFilterChng}
+            className={input}
+            value={filter}
+          />
+        </label>
+      )}
+
+      <h2 className={title}>Contact list </h2>
+      <TransitionGroup component="ul" className={list}>
+        {showList.map(({ id, name, phone }) => (
+          <CSSTransition timeout={250} classNames={fadeStyle} key={id}>
+            <li className={listItem}>
+              <span className={nameItem}>{name} :</span>
+              {phone}
+              <button
+                type="button"
+                onClick={onDeletBtn}
+                id={id}
+                className={button}
+              >
+                Delete
+              </button>
+            </li>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        filterHndl: (search) => dispatch(filterHndl(search)),
-        deletBtnHndl: (dltId) => dispatch(deletBtnHndl(dltId)),
-        getContactFromLS: (listLS) => dispatch(getContactFromLS(listLS))
-    }
-}
+const mapStateToProps = state => {
+  return {
+    showList: state.contactList.filter(item =>
+      item.name.toLowerCase().includes(state.filter.toLowerCase()),
+    ),
+    filter: state.filter,
+    contacts: state.contactList,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    filterHndl: search => dispatch(filterHndl(search)),
+    deletBtnHndl: dltId => dispatch(deletBtnHndl(dltId)),
+    getContactFromLS: listLS => dispatch(getContactFromLS(listLS)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
--- src/components/phoneBookForm/PhoneBookForm.js
+++ src/components/phoneBookForm/PhoneBookForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState} from 'react';
 import { connect } from "react-redux";
 import { CSSTransition } from 'react-transition-group';
 import { addContact } from "../../redux/actions/phoneBookAction";
